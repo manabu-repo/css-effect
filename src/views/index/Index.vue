dfea2f11d39bb720b2e17cc2e119cd9d110e204f
Akashi <akashi_sai@163.com>
2022-04-17 17:22:00 +0800

    :art: feat: support multi-entry

---
+++ src/views/index/Index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const count = ref(0);
+
+const onClick = () => {
+  count.value++;
+};
+</script>
+
+<template>
+  <div>
+    <span>{{ count }}</span>
+  </div>
+  <button @click="onClick">count</button>
+</template>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,24 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+
+  pages: {
+    index: {
+      entry: 'src/views/index/main.ts',
+      template: 'public/index.html',
+      filename: 'index.html',
+      title: 'index',
+      // 在这个页面中包含的块，默认情况下会包含
+      // 提取出来的通用 chunk 和 vendor chunk。
+      chunks: ['chunk-vendors', 'chunk-common', 'index'],
+    },
+
+    star: {
+      entry: 'src/views/star/main.ts',
+      template: 'public/index.html',
+      filename: 'star.html',
+      title: 'star',
+      chunks: ['chunk-vendors', 'chunk-common', 'star'],
+    },
+  },
+});
